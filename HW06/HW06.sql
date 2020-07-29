a6a88d70fb90f7cc6d8a85a413f95676c4afe81c
Altynbaev_Fanis <fanis@mail.ru>
2020-07-29 21:23:20 +0400

    HW06

--- HW06/HW06.sql
+++ HW06/HW06.sql
@@ -1,4 +1,8 @@
 --Группировки и агрегатные функции
+
+--Опционально:
+--Написать запросы 1-3 так, чтобы если в каком-то месяце не было продаж, то этот месяц также отображался бы в результатах, но там были нули.
+
 --1. Посчитать среднюю цену товара, общую сумму продажи по месяцам
 --Вывести:
 --* Год продажи 
@@ -16,6 +20,46 @@ join Sales.InvoiceLines il on il.InvoiceID = i.InvoiceID
 group by Year(i.InvoiceDate), Month(i.InvoiceDate)
 order by Year(i.InvoiceDate) desc, Month(i.InvoiceDate) desc
 
+-- опциональное решение
+;with MinDate(Value) As 
+(select DATEFROMPARTS(year(min(InvoiceDate)),month(min(InvoiceDate)), 1) 
+  from Sales.Invoices
+)
+,MaxDate(Value) As 
+(select DateAdd(m,1,DATEFROMPARTS(year(max(InvoiceDate)),month(max(InvoiceDate)), 1)) 
+   from Sales.Invoices
+)
+, AllMonth(Date, Month, Year) As
+(select MinDate.Value
+      , month(MinDate.Value)
+	  , year(MinDate.Value)
+  from MinDate
+union all
+ select       DateAdd(m,1,AllMonth.Date)
+      , month(DateAdd(m,1,AllMonth.Date))
+	  , year (DateAdd(m,1,AllMonth.Date))
+  from AllMonth, MaxDate
+  where /*AllMonth.Date < MaxDate.Value--*/DateAdd(m,1,AllMonth.Date) < MaxDate.Value
+)
+,MyInvoice (Year, Month, AVG, SUM) As
+(select 
+  Year(i.InvoiceDate)
+, Month(i.InvoiceDate)
+, AVG(il.UnitPrice) as AVGPrice
+, sum(il.Quantity * il.UnitPrice) as TotalSumm
+from Sales.Invoices i
+join Sales.InvoiceLines il on il.InvoiceID = i.InvoiceID
+group by Year(i.InvoiceDate), Month(i.InvoiceDate)
+)
+select 
+  am.Year
+, am.Month
+, isnull(i.AVG,0) as AVGPrice
+, isnull(i.sum,0) as TotalSumm
+from AllMonth am
+left join MyInvoice i on i.Year = am.Year and i.Month = am.Month
+order by am.Year desc, am.Month desc
+
 
 --2. Отобразить все месяцы, где общая сумма продаж превысила 10 000 
 --Вывести:
@@ -23,6 +67,8 @@ order by Year(i.InvoiceDate) desc, Month(i.InvoiceDate) desc
 --* Месяц продажи
 --* Общая сумма продаж
 --Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+/****************/
+-- !!! позволил себе изменить порог вхождения до 4 млн
 select 
   Year(i.InvoiceDate)
 , Month(i.InvoiceDate)
@@ -30,8 +76,47 @@ select
 from Sales.Invoices i
 join Sales.InvoiceLines il on il.InvoiceID = i.InvoiceID
 group by Year(i.InvoiceDate), Month(i.InvoiceDate)
-having sum(il.Quantity * il.UnitPrice)  > 10000
+having sum(il.Quantity * il.UnitPrice)  > 4000000--10000
+order by 1,2
 
+-- опциональное решение
+;with MinDate(Value) As 
+(select DATEFROMPARTS(year(min(InvoiceDate)),month(min(InvoiceDate)), 1) 
+  from Sales.Invoices
+)
+,MaxDate(Value) As 
+(select DateAdd(m,1,DATEFROMPARTS(year(max(InvoiceDate)),month(max(InvoiceDate)), 1)) 
+   from Sales.Invoices
+)
+, AllMonth(Date, Month, Year) As
+(select MinDate.Value
+      , month(MinDate.Value)
+	  , year(MinDate.Value)
+  from MinDate
+union all
+ select       DateAdd(m,1,AllMonth.Date)
+      , month(DateAdd(m,1,AllMonth.Date))
+	  , year (DateAdd(m,1,AllMonth.Date))
+  from AllMonth, MaxDate
+  where /*AllMonth.Date < MaxDate.Value--*/DateAdd(m,1,AllMonth.Date) < MaxDate.Value
+)
+,MyInvoice (Year, Month, TotalSumm) As
+(select 
+  Year(i.InvoiceDate)
+, Month(i.InvoiceDate)
+, sum(il.Quantity * il.UnitPrice)
+from Sales.Invoices i
+join Sales.InvoiceLines il on il.InvoiceID = i.InvoiceID
+group by Year(i.InvoiceDate), Month(i.InvoiceDate)
+having sum(il.Quantity * il.UnitPrice)  > 4000000--10000
+)
+select 
+  am.Year
+, am.Month
+, isnull(i.TotalSumm,0)
+from AllMonth am
+left join MyInvoice i on i.Year = am.Year and i.Month = am.Month
+order by 1,2
 
 --3. Вывести сумму продаж, дату первой продажи и количество проданного по месяцам, 
 --по товарам, продажи которых менее 50 ед в месяц. 
@@ -44,7 +129,7 @@ having sum(il.Quantity * il.UnitPrice)  > 10000
 --* Дата первой продажи
 --* Количество проданного
 --Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
-select --top 10 
+select 
   Year(i.InvoiceDate) as Year
 , Month(i.InvoiceDate) as Month
 , il.Description as Description
@@ -55,10 +140,59 @@ from Sales.Invoices i
 join Sales.InvoiceLines il on il.InvoiceID = i.InvoiceID
 group by Year(i.InvoiceDate), Month(i.InvoiceDate), il.Description
 having sum(il.Quantity) < 50
+order by 1,2,3
+
+
+-- опциональное решение
+;with MinDate(Value) As 
+(select DATEFROMPARTS(year(min(InvoiceDate)),month(min(InvoiceDate)), 1) 
+  from Sales.Invoices
+)
+,MaxDate(Value) As 
+(select DateAdd(m,1,DATEFROMPARTS(year(max(InvoiceDate)),month(max(InvoiceDate)), 1)) 
+   from Sales.Invoices
+)
+, AllMonth(Date, Month, Year) As
+(select MinDate.Value
+      , month(MinDate.Value)
+	  , year(MinDate.Value)
+  from MinDate
+union all
+ select       DateAdd(m,1,AllMonth.Date)
+      , month(DateAdd(m,1,AllMonth.Date))
+	  , year (DateAdd(m,1,AllMonth.Date))
+  from AllMonth, MaxDate
+  where /*AllMonth.Date < MaxDate.Value--*/DateAdd(m,1,AllMonth.Date) < MaxDate.Value
+)
+,MyInvoice As
+(select 
+  Year(i.InvoiceDate) as Year
+, Month(i.InvoiceDate) as Month
+, il.Description as Description
+, sum(il.Quantity * il.UnitPrice) as TotalSumm
+, min(i.InvoiceDate) as min_InvoiceDate
+, sum(il.Quantity) as Quantity
+from Sales.Invoices i
+join Sales.InvoiceLines il on il.InvoiceID = i.InvoiceID
+group by Year(i.InvoiceDate), Month(i.InvoiceDate), il.Description
+having sum(il.Quantity) < 50
+)
+select 
+  am.Year
+, am.Month
+, isnull(i.Description,'')
+, isnull(TotalSumm, 0)
+, isnull(min_InvoiceDate, '')
+, isnull(Quantity, 0)
+from AllMonth am
+left join MyInvoice i on i.Year = am.Year and i.Month = am.Month
+order by 1,2,3
+
 
 
 --4. Написать рекурсивный CTE sql запрос и заполнить им временную таблицу и табличную переменную
 --Дано :
+DROP TABLE dbo.MyEmployees
 CREATE TABLE dbo.MyEmployees 
 ( 
 EmployeeID smallint NOT NULL, 
@@ -86,78 +220,93 @@ select * from dbo.MyEmployees
 --временная таблица
 drop table #MyEmployees
 CREATE TABLE #MyEmployees 
-( EmployeeID smallint     NOT NULL, 
+( Level      smallint     NOT NULL, 
+  Sort       nvarchar(100),
+  EmployeeID smallint     NOT NULL, 
+  ManagerID  smallint     NOT NULL, 
   Name       nvarchar(80) NOT NULL, 
-  Title      nvarchar(50) NOT NULL, 
-  Level      smallint     NOT NULL 
+  Title      nvarchar(50) NOT NULL 
 ); 
 
-with Tr
-( EmployeeID,
+;with Tr
+( Level,
+  Sort,
+  EmployeeID,
+  ManagerID ,
   Name      ,
-  Title     ,
-  Level     
+  Title     
 )as
-(select EmployeeID                 as EmployeeID
-      , FirstName + ' ' + LastName as Name
-	  , Title                      as Title
-	  , 1                          as Level
+(select 1,
+        cast(EmployeeID as varchar(max)) as Sort,
+        EmployeeID                 as EmployeeID,
+        0                          as ManagerID,
+        FirstName + ' ' + LastName as Name,
+	    Title                      as Title
    from dbo.MyEmployees
   where ManagerID is null
 
   union all
 
- select e.EmployeeID                
-      , e.FirstName + ' ' + e.LastName
-	  , e.Title                     
-	  , tr.Level + 1 
+ select tr.Level+1,
+        Tr.Sort+' ' + cast(e.EmployeeID as varchar(max)),
+        e.EmployeeID,  
+	    e.ManagerID,	
+        e.FirstName + ' ' + e.LastName,
+	    e.Title                     
    from dbo.MyEmployees e
    join Tr on Tr.EmployeeID = e.ManagerID
 )
-insert into #MyEmployees (EmployeeID, Name, Title, Level)
+insert into #MyEmployees (Level, Sort, EmployeeID, ManagerID, Name, Title)
 select *
 from Tr
+order by Sort
 
-select * from #MyEmployees
+select EmployeeID,	Name,	Title, Level
+from #MyEmployees
+order by Sort
 
 
 -- табличная переменная
 DECLARE @MyEmployees table(
+  Level      smallint     NOT NULL, 
+  Sort       nvarchar(100),
   EmployeeID smallint     NOT NULL, 
+  ManagerID  smallint     NOT NULL,
   Name       nvarchar(80) NOT NULL, 
-  Title      nvarchar(50) NOT NULL, 
-  Level      smallint     NOT NULL 
+  Title      nvarchar(50) NOT NULL
 ); 
 
-with Tr
-( EmployeeID,
+;with Tr
+( Level,
+  Sort,
+  EmployeeID,
+  ManagerID ,
   Name      ,
-  Title     ,
-  Level     
+  Title     
 )as
-(select EmployeeID                 as EmployeeID
-      , FirstName + ' ' + LastName as Name
-	  , Title                      as Title
-	  , 1                          as Level
+(select 1,
+        cast(EmployeeID as varchar(max)) as Sort,
+        EmployeeID                 as EmployeeID,
+        0                          as ManagerID,
+        FirstName + ' ' + LastName as Name,
+	    Title                      as Title
    from dbo.MyEmployees
   where ManagerID is null
 
   union all
 
- select e.EmployeeID                
-      , e.FirstName + ' ' + e.LastName
-	  , e.Title                     
-	  , tr.Level + 1 
+ select tr.Level+1,
+        Tr.Sort+' ' + cast(e.EmployeeID as varchar(max)),
+        e.EmployeeID,  
+	    e.ManagerID,	
+        e.FirstName + ' ' + e.LastName,
+	    e.Title                     
    from dbo.MyEmployees e
    join Tr on Tr.EmployeeID = e.ManagerID
 )
-insert into @MyEmployees (EmployeeID, Name, Title, Level)
+insert into @MyEmployees (Level, Sort, EmployeeID, ManagerID, Name, Title)
 select *
 from Tr
 
-select * from @MyEmployees
-
-
---Опционально:
---Написать запросы 1-3 так, чтобы если в каком-то месяце не было продаж, 
---то этот месяц также отображался бы в результатах, но там были нули.
+select EmployeeID,	Name,	Title, Level
+from @MyEmployees order by Sort
